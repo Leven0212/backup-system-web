b265aa8af6a554024a8212976ba187a8693625d5
leven <leven353@gmail.com>
2022-10-25 21:03:36 +0800

    Initial commit

---
+++ src/main/java/com/backup/backupsystemweb/BackupSystemWebApplication.java
@@ -0,0 +1,13 @@
+package com.backup.backupsystemweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupSystemWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupSystemWebApplication.class, args);
+	}
+
+}
